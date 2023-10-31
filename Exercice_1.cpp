7c9d7cc4528c65ddff4f0552687141121120cf20
Dodo2211 <doriandiana@outlook.fr>
2023-10-31 10:54:42 +0100

    Exercice n°1 OK

---
+++ Exercice_1.cpp
@@ -0,0 +1,12 @@
+// Exercice n°1 
+// Écrire un programme qui affiche le message suivant à l'execution
+// $ ./a.out
+//Hello World from 60 boulmich
+#include<iostream>
+
+int main(){
+    std::cout<< "Hello World from 60 boulmich"<<std::endl;
+    return 0;
+}
+
+//OK
