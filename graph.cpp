2f8c09b9fec0fdf4bf57cb285614ed08bc63bc5b
Dodo2211 <doriandiana@outlook.fr>
2023-12-15 19:05:20 +0100

    TP graph affichage + deep_search 15-12-23

--- graph.cpp
+++ graph.cpp
@@ -1,14 +1,23 @@
 #include"graph.hpp"
 
+
 int main(){
     Graph g;
     g.add_vertex(0);
     g.add_vertex(1);
     g.add_vertex(2);
+    g.add_vertex(3);
+    g.add_vertex(4);
     g.add_edge(0,0,1);
+    //g.add_edge(0,0,5.2);
     g.add_edge(0,1,0.2); 
     g.add_edge(0,2,0.3);
     g.add_edge(1,0,3.2);
+    g.add_edge(1,2,3);
+    g.add_edge(3,4,0.2);
+    g.deep_search();
     g.print();
+    g.print_graph();
+    
     return(0);
 }
--- graph.hpp
+++ graph.hpp
@@ -1,15 +1,24 @@
+//pour une taille type  std::size_t
+//friend dans les classe pour avoir accees au elements private
+#pragma once //inclusion une seule fois
 #include<iostream>
 #include<vector>
+#include<cmath>
+#include<fstream>
+#include"termcolor.hpp"
+
 
 using namespace std;
-struct Edge
+class Edge
 {
 private:
-public:
+friend class Graph;
+friend class Vertex;
     float value;
     int num;
     Edge( )=default;
     Edge(float val,int etatf);
+public:
 
 };
 inline Edge::Edge(float val,int etatf){
@@ -20,21 +29,37 @@ inline Edge::Edge(float val,int etatf){
 struct Vertex
 {
 private:  
-public:
+friend class Graph;
     vector<Edge> edges;
     int num;
     Vertex( ) = default;
     Vertex(int numero);
+    void deep_search(vector<Vertex> &v,vector<bool> &visit);
+public:
+    
 };
 inline Vertex::Vertex(int numero){
     num=numero;
 }
+inline void Vertex::deep_search(vector<Vertex> &v, vector<bool> &visit){
+    //vector<bool>visit_arret(edges.size(),false);
+    for(Edge& e:edges){
+        cout<<termcolor::bright_red <<e.value<<"\t";
+        //if(not visit_arret[])
+        if(not visit[e.num]){
+            visit[e.num]=true;
+            cout<<termcolor::bright_yellow <<e.num<<"\t";
+            v[e.num].deep_search(v,visit);
+        }
+    }
+}
 
-struct Graph
+class Graph
 {
-private:  
+private: 
+    vector<Vertex> vertices; 
 public:
-    vector<Vertex> vertices;
+    
     ~Graph() = default;
     void add_vertex(int num){
         vertices.push_back(Vertex(num));
@@ -43,15 +68,46 @@ public:
         vertices[from].edges.push_back(Edge(val,to));
     }
     void print(){
-        cout<<"etat courant \t valeur/etat futur"<<endl;
+        cout<<termcolor::reset <<"etat courant \t valeur/etat futur"<<endl;
         for(int i=0; i<vertices.size();i++){
             for(int j=0;j<vertices[i].edges.size();j++){
-                cout<<vertices[i].num<<" -> \t"<<vertices[i].edges[j].value<<"/"<<vertices[i].edges[j].num<<endl;
+                cout<<termcolor::reset<<vertices[i].num<<" -> \t"<<vertices[i].edges[j].value<<"/"<<vertices[i].edges[j].num<<endl;
             }
             
         }
     }
+    void print_graph(void);
+    void deep_search(void);
 };
 
+inline void Graph::deep_search(void){
+    vector<bool>visit(vertices.size(),false);
+    //visit.reserve(vertices.size());
+    for(Vertex& v : vertices){
+        if(not visit[v.num]){
+            visit[v.num]=true;
+            cout<<termcolor::bright_yellow <<v.num<<"\t";
+            v.deep_search(vertices,visit);
 
+        }
+    }
+    cout<<endl;
+}
 
+inline void Graph::print_graph(void){
+    string const mon_fichier("graph_print.dot");
+    ofstream myfile(mon_fichier);
+    myfile<<"digraph G{"<<endl;
+    for(int i=0 ; i<vertices.size();i++){
+        myfile<<vertices[i].num<<" [label=\""<<vertices[i].num<<"\"]"<<endl;
+    }
+    for(int i=0; i<vertices.size();i++){
+        for(int j=0;j<vertices[i].edges.size();j++){
+            myfile<<vertices[i].num<<" -> "<<vertices[i].edges[j].num<<"[label=\""<<vertices[i].edges[j].value<<"\"]"<<endl;
+        }    
+    }
+    myfile<<"}";
+    myfile.close();
+    system("dot -Tsvg graph_print.dot > graph.svg");
+    system("display graph.svg");
+}
